72875217bb31937ff17da7c3658120b615f08146
Volodimir <volodimir.dovgan@hotmail.com>
2024-11-19 13:58:42 +0200

    svTest5

---
+++ FitnessApp.Common.IntegrationTests/Fixtures/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace FitnessApp.Common.IntegrationTests.Fixtures;
+
+public static class MockConstants
+{
+    public const string Scheme = "TestAuth";
+    public const string SvTest = "svTest";
+}
+
+public static class IdsConstants
+{
+    public const string IdToGet = nameof(IdToGet);
+    public const string IdToCreate = nameof(IdToCreate);
+    public const string IdToUpdate = nameof(IdToUpdate);
+    public const string IdToDelete = nameof(IdToDelete);
+
+    public static string[] IdsToSeed { get; } = [
+        IdToGet,
+        IdToUpdate,
+        IdToDelete
+    ];
+}
--- FitnessApp.Common.IntegrationTests/Fixtures/MockConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FitnessApp.Common.IntegrationTests.Fixtures;
-
-public static class MockConstants
-{
-    public const string Scheme = "TestAuth";
-    public const string SvTest = "svTest";
-}
